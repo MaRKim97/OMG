1acaaedfaec6df8cb021a08c83f68ecce4672925
PC1 <gmdcjs0717@gmail.com>
2024-01-26 13:51:47 +0900

    2024.01.26

--- job01_movie_project.py
+++ job01_movie_project.py
@@ -1,4 +1,5 @@
 # OMG에 오신 여러분 환영합니다
+import numpy as np
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options as ChromeOptions
@@ -7,9 +8,9 @@ from selenium.common.exceptions import NoSuchElementException, StaleElementRefer
 import pandas as pd
 import re
 import time
-import datetimeW
+import datetime
 
-categories = ['animation']
+categories = ['animation', 'action']
 
 options = ChromeOptions()
 service = ChromeService(executable_path=ChromeDriverManager().install())
@@ -30,7 +31,8 @@ for category in categories:
 
     except:
         print('driver.get', category)
-    for j in range(34):
+
+    for j in range(10):
         driver.find_element('xpath', '//*[@id="content"]/div[2]/button').click()
         time.sleep(0.5)
         for i in range(j*30+1, j*30+31, 30):
@@ -40,27 +42,40 @@ for category in categories:
                 print(title)
                 driver1.get(title)
                 time.sleep(0.5)
-                print(title)
                 temp = driver1.find_element('xpath', '//*[@id="content"]/div[2]/div/div[1]/div[1]/strong').text
                 titles_real.append(temp)
+                # titles.append(title)
                 text = driver1.find_element('xpath', '//*[@id="content"]/div[2]/ul/li[1]/div[3]/p').text
                 text = re.compile('[^가-힇]').sub(' ', text)
-                titles.append(title)
                 texts.append(text)
                 driver1.close()
             except:
-                print('find element', category, i)
+                try:
+                    text = driver1.find_element('xpath', '//*[@id="content"]/div[2]/ul/li[1]/div[2]/p').text
+                    text = re.compile('[^가-힇]').sub(' ', text)
+                    texts.append(text)
+                    print('find element', category, i)
+                except:
+                    texts.append('NULL')
+                    titles_real.append('NULL')
+                    print('real NULL', category, i)
                 driver1.close()
 
+    # print(len(titles_real))
+    # print(len(texts))
+    df_section_titles = pd.DataFrame()
+    df_section_titles["title"] = titles_real
+    df_section_titles["text"] = texts
+    df_section_titles["category"] = category
 
-    df_section_titles = pd.DataFrame({'title':titles_real, 'text':texts, 'category':categories})
+    # df_section_titles = pd.DataFrame({'title':titles_real, 'text':texts, 'category':categories})
     df_titles = pd.concat([df_titles, df_section_titles], axis='rows', ignore_index=True)
 df_titles.to_csv('./crawling_data/data.csv')
 
 driver.close()
 # print(texts)
 
-
+# //*[@id="content"]/div[2]/ul/li[1]/div[2]/p
 
 # //*[@id="content"]/div[1]/ul/li[32]/a
 
