ae132bd22822bfff15bc15c57bf3c3b1deddb74e
OThinking <149746918+OThinking@users.noreply.github.com>
2024-01-29 15:22:50 +0900

    Update job02_data_concat.py

--- job02_data_concat.py
+++ job02_data_concat.py
@@ -3,23 +3,17 @@ import pandas as pd
 import glob
 import datetime
 
-# last_data = []
-# for i in range(6):
-#     data_path = glob.glob('./crawling_data/data_{}*'.format(i))[-1]
-#     last_data.append(data_path)
-# print(last_data)
+# ↓ 제목, 줄거리, 카테고리를 포함한 모든 데이터 불러오기 ↓
+data_path = glob.glob("./crawling_data/*.csv")
 
-data_path = glob.glob("./crawling_data/data2.csv")
-print(data_path)
-
+# ↓ 데이터를 합쳐주는 과정 ↓
 df = pd.DataFrame()
 for path in data_path:
     df_temp = pd.read_csv(path, index_col=0)
-    df = pd.concat([df, df_temp], axis = "rows", ignore_index = True)
+    df = pd.concat([df, df_temp], axis="rows", ignore_index=True)
 df = df.replace('NULL', np.nan)
 df = df.dropna()
 
-print(df.head())
 print(df["category"].value_counts())
 print(df.info())
-df.to_csv("./movie_concat_data_{}.csv".format(datetime.datetime.now().strftime("%Y%m%d")), index = False)
+df.to_csv("./movie_concat_data_{}.csv".format(datetime.datetime.now().strftime("%Y%m%d")), index=False)
